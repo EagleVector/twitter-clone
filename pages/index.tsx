f768cb8c4f4a9526dee9e59b2496f25ec4f92cef
EagleVector <kumarsubhambili3@gmail.com>
2023-08-28 22:23:04 +0530

    Rendering the Profile Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image';
 import { BsTwitter, BsBell, BsEnvelope, BsBookmark } from 'react-icons/bs';
 import { BiHomeCircle, BiHash, BiUser, BiMoney } from 'react-icons/bi';
-import {CiCircleMore} from 'react-icons/ci'
+import { CiCircleMore } from 'react-icons/ci';
 import FeedCard from '@/components/FeedCard';
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { useCallback } from 'react';
 import { toast } from 'react-hot-toast';
 import { verifyUserGoogleTokenQuery } from '@/graphql/query/user';
 import { graphqlClient } from '@/clients/api';
-
-
+import { useCurrentUser } from '@/hooks/user';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface TwitterSidebarButton {
 	title: string;
@@ -52,28 +52,36 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 ];
 
 export default function Home() {
+	const { user } = useCurrentUser();
+
+	const queryClient = useQueryClient();
+
+	console.log(user);
 
 	const handleLoginWithGoogle = useCallback(
 		async (cred: CredentialResponse) => {
-		const googleToken = cred.credential
-		if (!googleToken) return toast.error(`Google Token Not Found`);
+			const googleToken = cred.credential;
+			if (!googleToken) return toast.error(`Google Token Not Found`);
 
-		const { verifyGoogleToken } = await graphqlClient.request(
-			verifyUserGoogleTokenQuery, 
-			{token: googleToken}
+			const { verifyGoogleToken } = await graphqlClient.request(
+				verifyUserGoogleTokenQuery,
+				{ token: googleToken }
 			);
 
 			toast.success('Verification Success');
 			console.log(verifyGoogleToken);
 
-			if(verifyGoogleToken) {
-				window.localStorage.setItem("__twitter_token", verifyGoogleToken);
+			if (verifyGoogleToken) {
+				window.localStorage.setItem('__twitter_token', verifyGoogleToken);
 			}
-	}, [])
+			await queryClient.invalidateQueries(['current-user']);
+		},
+		[queryClient]
+	);
 	return (
 		<div>
 			<div className="grid grid-cols-12 h-screen w-screen px-36">
-				<div className="col-span-3 pt-8 px-4">
+				<div className="col-span-3 pt-8 px-4 relative">
 					<div className="text-4xl h-fit w-fit hover:bg-gray-800 rounded-full p-2 cursor-pointer transition-all">
 						<BsTwitter />
 					</div>
@@ -96,7 +104,25 @@ export default function Home() {
 						</div>
 					</div>
 				</div>
-				<div className="col-span-6 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
+				{user && (
+					<div className="absolute bottom-5 flex gap-2 items-center bg-slate-900 p-3 rounded-xl">
+						{user && user.profileImageURL && (
+							<Image
+								className="rounded-full"
+								src={user?.profileImageURL}
+								alt="user-image"
+								height={40}
+								width={40}
+							/>
+						)}
+						<div>
+							<h3 className="text-xl cursor-pointer">
+								{user.firstName} {user.lastName}
+							</h3>
+						</div>
+					</div>
+				)}
+				<div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
 					<FeedCard />
 					<FeedCard />
 					<FeedCard />
@@ -108,11 +134,13 @@ export default function Home() {
 					<FeedCard />
 					<FeedCard />
 				</div>
-				<div className="col-span-3 p-5">
-					<div className='p-5 bg-slate-800 rounded-lg'>
-						<h1 className='my-2 text-xl'>New To Twitter?</h1>
-						<GoogleLogin onSuccess={handleLoginWithGoogle} />
-					</div>
+				<div className="col-span-4 p-5">
+					{!user && (
+						<div className="p-5 bg-slate-800 rounded-lg">
+							<h1 className="my-2 text-xl">New To Twitter?</h1>
+							<GoogleLogin onSuccess={handleLoginWithGoogle} />
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
