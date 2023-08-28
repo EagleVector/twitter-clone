10b88b2c7aff9abf4668b88425948cc5dfba63d7
EagleVector <kumarsubhambili3@gmail.com>
2023-08-28 22:19:37 +0530

    react query setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,25 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 
-
 const inter = Inter({ subsets: ['latin'] });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className={inter.className}>
-      <GoogleOAuthProvider clientId="991386349229-hb2b1fjfh2v80don6jb282dnbd311oan.apps.googleusercontent.com">
-        <Component {...pageProps} />
-        <Toaster />
-      </GoogleOAuthProvider>
-    </div>
-  );
+	return (
+		<div className={inter.className}>
+			<QueryClientProvider client={queryClient}>
+				<GoogleOAuthProvider clientId="991386349229-hb2b1fjfh2v80don6jb282dnbd311oan.apps.googleusercontent.com">
+					<Component {...pageProps} />
+					<Toaster />
+					<ReactQueryDevtools />
+				</GoogleOAuthProvider>
+			</QueryClientProvider>
+		</div>
+	);
 }
