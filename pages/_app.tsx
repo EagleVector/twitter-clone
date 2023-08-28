01ad527f4ec8b68600be02316e42ab5f12ef43de
EagleVector <kumarsubhambili3@gmail.com>
2023-08-28 15:47:00 +0530

    google oauth client side setup with react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={inter.className}><Component {...pageProps} /></div>
+  return (
+    <div className={inter.className}>
+      <GoogleOAuthProvider clientId="991386349229-hb2b1fjfh2v80don6jb282dnbd311oan.apps.googleusercontent.com">
+        <Component {...pageProps} />
+        <Toaster />
+      </GoogleOAuthProvider>
+    </div>
+  );
 }
