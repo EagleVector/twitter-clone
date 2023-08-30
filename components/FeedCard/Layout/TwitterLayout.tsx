17fc2fe2fa39e14b5a97074e6b85e61ceb2850d6
EagleVector <kumarsubhambili3@gmail.com>
2023-08-30 20:35:32 +0530

    Twitter Original Layout

---
+++ components/FeedCard/Layout/TwitterLayout.tsx
@@ -0,0 +1,143 @@
+import { useCurrentUser } from '@/hooks/user';
+import React, { useCallback } from 'react';
+import Image from 'next/image';
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from 'react-icons/bi';
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from 'react-icons/bs';
+import { CiCircleMore } from 'react-icons/ci';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import toast from 'react-hot-toast';
+import { graphqlClient } from '@/clients/api';
+import { verifyUserGoogleTokenQuery } from '@/graphql/query/user';
+import { useQueryClient } from '@tanstack/react-query';
+
+interface TwitterLayoutProps {
+	children: React.ReactNode;
+}
+
+interface TwitterSidebarButton {
+	title: string;
+	icon: React.ReactNode;
+}
+
+const sidebarMenuItems: TwitterSidebarButton[] = [
+	{
+		title: 'Home',
+		icon: <BiHomeCircle />
+	},
+	{
+		title: 'Explore',
+		icon: <BiHash />
+	},
+	{
+		title: 'Notifications',
+		icon: <BsBell />
+	},
+	{
+		title: 'Messages',
+		icon: <BsEnvelope />
+	},
+	{
+		title: 'Bookmarks',
+		icon: <BsBookmark />
+	},
+	{
+		title: 'Monetization',
+		icon: <BiMoney />
+	},
+	{
+		title: 'Profile',
+		icon: <BiUser />
+	},
+	{
+		title: 'More Options',
+		icon: <CiCircleMore />
+	}
+];
+
+const TwitterLayout: React.FC<TwitterLayoutProps> = props => {
+	const { user } = useCurrentUser();
+
+  const queryClient = useQueryClient();
+
+	const handleLoginWithGoogle = useCallback(
+		async (cred: CredentialResponse) => {
+			const googleToken = cred.credential;
+			if (!googleToken) return toast.error(`Google Token Not Found`);
+
+			const { verifyGoogleToken } = await graphqlClient.request(
+				verifyUserGoogleTokenQuery,
+				{ token: googleToken }
+			);
+
+			toast.success('Verification Success');
+			console.log(verifyGoogleToken);
+
+			if (verifyGoogleToken) {
+				window.localStorage.setItem('__twitter_token', verifyGoogleToken);
+			}
+			await queryClient.invalidateQueries(['current-user']);
+		},
+		[queryClient]
+	);
+
+	return (
+		<div>
+			<div className="grid grid-cols-12 h-screen w-screen px-36">
+				<div className="col-span-3 pt-8 px-4 relative">
+					<div className="text-4xl h-fit w-fit hover:bg-gray-800 rounded-full p-2 cursor-pointer transition-all">
+						<BsTwitter />
+					</div>
+					<div className="mt-4 text-1xl pr-4">
+						<ul>
+							{sidebarMenuItems.map(item => (
+								<li
+									className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer mt-2"
+									key={item.title}
+								>
+									<span>{item.icon}</span>
+									<span>{item.title}</span>
+								</li>
+							))}
+						</ul>
+						<div className="mt-5 px-3">
+							<button className="bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full">
+								Tweet
+							</button>
+						</div>
+					</div>
+				</div>
+				{user && (
+					<div className="absolute bottom-5 flex gap-2 items-center bg-slate-900 p-3 rounded-xl">
+						{user && user.profileImageURL && (
+							<Image
+								className="rounded-full"
+								src={user?.profileImageURL}
+								alt="user-image"
+								height={40}
+								width={40}
+							/>
+						)}
+						<div>
+							<h3 className="text-xl cursor-pointer">
+								{user.firstName} {user.lastName}
+							</h3>
+						</div>
+					</div>
+				)}
+				<div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
+					{props.children}
+				</div>
+				<div className="col-span-4 p-5">
+					{!user && (
+						<div className="p-5 bg-slate-800 rounded-lg">
+							<h1 className="my-2 text-xl">New To Twitter?</h1>
+							<GoogleLogin onSuccess={handleLoginWithGoogle} />
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TwitterLayout;
