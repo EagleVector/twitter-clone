c7916a2ca72b6f41ffd88e6bd60b96f2cf4815c1
EagleVector <kumarsubhambili3@gmail.com>
2023-08-30 18:02:41 +0530

    Rendering the user generated tweet feeds

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,14 @@ import {
 import { CiCircleMore } from 'react-icons/ci';
 import FeedCard from '@/components/FeedCard';
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { verifyUserGoogleTokenQuery } from '@/graphql/query/user';
 import { graphqlClient } from '@/clients/api';
 import { useCurrentUser } from '@/hooks/user';
 import { useQueryClient } from '@tanstack/react-query';
+import { useCreateTweet, useGetAllTweets } from '@/hooks/tweet';
+import { Tweet } from '@/gql/graphql';
 
 interface TwitterSidebarButton {
 	title: string;
@@ -59,15 +61,27 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 
 export default function Home() {
 	const { user } = useCurrentUser();
+	const { tweets = [] } = useGetAllTweets();
+	const { mutate } = useCreateTweet()
 
 	const queryClient = useQueryClient();
+
+	const [content, setContent] = useState("");
 
 	const handleSelectImage = useCallback(() => {
 		const input = document.createElement("input");
 		input.setAttribute("type", "file");
 		input.setAttribute("accept", "image/*");
 		input.click();
-	}, [])
+	}, []);
+
+	const handleCreateTweet = useCallback(() => {
+		mutate({
+			content,
+		})
+		setContent("");
+	}, [content, mutate])
+
 
 	const handleLoginWithGoogle = useCallback(
 		async (cred: CredentialResponse) => {
@@ -89,6 +103,8 @@ export default function Home() {
 		},
 		[queryClient]
 	);
+
+
 	return (
 		<div>
 			<div className="grid grid-cols-12 h-screen w-screen px-36">
@@ -135,9 +151,9 @@ export default function Home() {
 				)}
 				<div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
 					<div>
-						<div className="border border-l-0 border-r-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer"></div>
+						<div className="border border-l-0 border-r-0 border-b-0 border-gray-600 p-3 hover:bg-slate-900 transition-all cursor-pointer"></div>
 						<div className="grid grid-cols-12 gap-1">
-							<div className="col-span-1">
+							<div className="col-span-2 p-3">
 								{user?.profileImageURL && (
 									<Image
 										className="rounded-full"
@@ -148,31 +164,28 @@ export default function Home() {
 									/>
 								)}
 							</div>
-							<div className="col-span-11">
+							<div className="col-span-10">
 								<textarea
+									value={content}
+									onChange={(e) => setContent(e.target.value)}
 									className="border w-full bg-transparent text-xl px-3 border-b border-slate-700"
 									placeholder="What's happening?"
 									rows={3}
 								></textarea>
 								<div className="mt-2 mb-3 flex justify-between items-center">
 									<BiImageAlt onClick={handleSelectImage} className="text-xl cursor-pointer" />
-									<button className="bg-[#1d9bf0] font-semibold text-sm py-2 px-4 rounded-full cursor-pointer">
+									<button onClick={handleCreateTweet} className="bg-[#1d9bf0] font-semibold text-sm py-2 px-4 rounded-full cursor-pointer">
 										Tweet
 									</button>
 								</div>
 							</div>
 						</div>
 					</div>
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
-					<FeedCard />
+					{
+						tweets?.map((tweet) =>
+							tweet ? <FeedCard key={tweet?.id} data={tweet as Tweet} /> : null 
+						)}
+						
 				</div>
 				<div className="col-span-4 p-5">
 					{!user && (
