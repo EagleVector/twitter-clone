e228ad4ea2ea10fcf7f2f3bac909112076b04a9e
EagleVector <kumarsubhambili3@gmail.com>
2023-09-01 18:12:54 +0530

    Rendering the image on the feedcard

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,58 +1,72 @@
-import React from "react";
+import React from 'react';
 import Image from 'next/image';
-import { BiMessageRounded, BiRepost, BiBarChart, BiUpload } from 'react-icons/bi';
+import {
+	BiMessageRounded,
+	BiRepost,
+	BiBarChart,
+	BiUpload
+} from 'react-icons/bi';
 import { BsHeart } from 'react-icons/bs';
-import { Tweet } from "@/gql/graphql";
-import Link from "next/link";
+import { Tweet } from '@/gql/graphql';
+import Link from 'next/link';
 
 interface FeedCardProps {
-  data: Tweet
+	data: Tweet;
 }
 
-const FeedCard: React.FC<FeedCardProps> = (props) => {
+const FeedCard: React.FC<FeedCardProps> = props => {
+	const { data } = props;
 
-  const { data } = props
-
-  return (
-    <div className="border border-l-0 border-r-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
-      <div className="grid grid-cols-12 gap-1">
-        <div className="col-span-1">
-          {data.author?.profileImageURL && <Image 
-            className="rounded-full"
-            src={data.author?.profileImageURL} 
-            alt="user-image" 
-            height={50} 
-            width={50}
-          />}
-        </div>
-        <div className="col-span-11">
-          <h5>
-            <Link href={`/${data.author?.id}`}>
-              {data.author?.firstName} {data.author?.lastName}
-            </Link>
-          </h5>
-          <p>{data.content}</p>
-          <div className="flex justify-between mt-5 text-xl items-center p-2 w-[85%]">
-            <div>
-              <BiMessageRounded />
-            </div>
-            <div>
-              <BiRepost />
-            </div>
-            <div>
-              <BsHeart />
-            </div>
-            <div>
-              <BiBarChart />
-            </div>
-            <div>
-              <BiUpload />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="border border-l-0 border-r-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+			<div className="grid grid-cols-12 gap-1">
+				<div className="col-span-1">
+					{data.author?.profileImageURL && (
+						<Image
+							className="rounded-full"
+							src={data.author?.profileImageURL}
+							alt="user-image"
+							height={50}
+							width={50}
+						/>
+					)}
+				</div>
+				<div className="col-span-11">
+					<h5>
+						<Link href={`/${data.author?.id}`}>
+							{data.author?.firstName} {data.author?.lastName}
+						</Link>
+					</h5>
+					<p>{data.content}</p>
+					{data.imageURL && (
+						<Image
+							src={data.imageURL}
+							alt="image"
+							width={400}
+							height={400}
+						/>
+					)}
+					<div className="flex justify-between mt-5 text-xl items-center p-2 w-[85%]">
+						<div>
+							<BiMessageRounded />
+						</div>
+						<div>
+							<BiRepost />
+						</div>
+						<div>
+							<BsHeart />
+						</div>
+						<div>
+							<BiBarChart />
+						</div>
+						<div>
+							<BiUpload />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default FeedCard;
